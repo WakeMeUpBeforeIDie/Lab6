cdc8d8e343eeaf50c6ef68e3734fc96fb7a25b69
AutumnAsh <aleksandr.kasyanov1337@mail.ru>
2026-05-04 13:42:19 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,21 @@
 int main(int argc, char* argv[]) {
     std::string inputFile = "input.txt";
     std::string outputFile = "gen.txt";
-    const int NPREF = 2; 
+    const int NPREF = 2;
     const int MAXGEN = 1000;
-    
+
     if (argc > 1) {
         inputFile = argv[1];
     }
     if (argc > 2) {
         MAXGEN = std::stoi(argv[2]);
     }
-    
+
     std::cout << "Генератор текста на основе цепи Маркова" << std::endl;
     std::cout << "Размер префикса: " << NPREF << " слов" << std::endl;
     std::cout << "Объем генерации: " << MAXGEN << " слов" << std::endl;
     std::cout << "Входной файл: " << inputFile << std::endl;
-    
+
     TextGenerator generator(NPREF, MAXGEN);
     std::cout << "Чтение файла и построение таблицы..." << std::endl;
     if (!generator.buildTable(inputFile)) {
@@ -31,12 +31,13 @@ int main(int argc, char* argv[]) {
     std::cout << "Генерация текста..." << std::endl;
     std::string generatedText = generator.generate();
     if (generator.saveToFile(outputFile, generatedText)) {
-        std::cout << "Текст успешно сгенерирован и сохранен в " << outputFile << std::endl;
-        std::cout << "\nСгенерированный текст (первые 500 символов):" << std::endl;
-        std::cout << generatedText.substr(0, std::min(size_t(500), generatedText.size())) << "..." << std::endl;
+        std::cout << "Текст сохранен в " << outputFile << std::endl;
+        std::cout << "\nTекст:" << std::endl;
+        std::cout << generatedText.substr(0, std::min(size_t(500), generatedText.size()));
+        std::cout << "..." << std::endl;
     } else {
         std::cerr << "Ошибка при сохранении результата!" << std::endl;
         return 1;
-    }    
+    } 
     return 0;
 }
