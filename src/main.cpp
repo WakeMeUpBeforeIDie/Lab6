af5a520a69bfe3b3e5ab6a00fa8e7b5efd3e8f37
AutumnAsh <aleksandr.kasyanov1337@mail.ru>
2026-05-04 13:58:13 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,12 @@ int main(int argc, char* argv[]) {
     std::string outputFile = "gen.txt";
     const int NPREF = 2;
     const int MAXGEN = 1000;
-
     if (argc > 1) {
         inputFile = argv[1];
     }
     if (argc > 2) {
         MAXGEN = std::stoi(argv[2]);
     }
-
     std::cout << "Генератор текста на основе цепи Маркова" << std::endl;
     std::cout << "Размер префикса: " << NPREF << " слов" << std::endl;
     std::cout << "Объем генерации: " << MAXGEN << " слов" << std::endl;
@@ -33,12 +31,9 @@ int main(int argc, char* argv[]) {
     std::string generatedText = generator.generate();
     if (generator.saveToFile(outputFile, generatedText)) {
         std::cout << "Текст сохранен в " << outputFile << std::endl;
-        std::cout << "\nTекст:" << std::endl;
-        std::cout << generatedText.substr(0, std::min(size_t(500), generatedText.size()));
-        std::cout << "..." << std::endl;
     } else {
         std::cerr << "Ошибка при сохранении результата!" << std::endl;
         return 1;
-    } 
+    }
     return 0;
 }
